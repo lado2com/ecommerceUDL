79cd982e56a6a90e6e1b30b96ff1613c4b8e7210
Adriana Santiago Contreras <adriana.santiago136656@gmail.com>
2025-07-05 11:30:04 -0600

    ya subi las 4  paginas faltantes: catalogo, producto y confirmacion

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.scss'],
+  standalone: true,
 })
-export class CatalogComponent  implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {}
-
-}
