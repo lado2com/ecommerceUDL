d4e09cd33be0a0f479a7f511798c8166853cc125
Benjamín Paredes Ramírez <benjamin.paredesr@outlook.com>
2025-07-05 08:55:24 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ecommerceUDL',
+  webDir: 'www'
+};
+
+export default config;
